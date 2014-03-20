0fb60bb29db7310b2bf3ed74a691a78e05b6b8ef
Celso Guido <celso.guido@samsung.com>
2014-03-20 18:56:33 -0400

    Subject: Chapter 12. Adding DialogFragment when the user clicks on date button. Developer: celso.guido

---
+++ CriminalIntent/src/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,25 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+
+public class DatePickerFragment extends DialogFragment {
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		return new AlertDialog.Builder(getActivity())
+			.setTitle(R.string.date_picker_title)
+			.setPositiveButton(android.R.string.ok, new OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+				}
+
+			}).create();
+	}
+}
